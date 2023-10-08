16e427d25dff338cdfbaaffee770839367b294f6
Weronika <WerRic96@protonmail.com>
2023-10-08 15:19:04 +0200

    Add Comment table

--- imagebook/models.py
+++ imagebook/models.py
@@ -8,3 +8,8 @@ class Image(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='images_posted')
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=255)
