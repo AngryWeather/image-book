682b2901692c122969018aee084a1bcc1bd53734
Weronika <WerRic96@protonmail.com>
2023-10-12 00:55:49 +0200

    Change navbar

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from .views import UserCreateView
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='user-register'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='user-login')
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='user-login'),
+    path('logout/', LogoutView.as_view(), name='user-logout')
 ]
