84fa362099e1c89cb9f292189cfc9a8d4c4d79c0
Weronika <WerRic96@protonmail.com>
2023-10-14 01:42:33 +0200

    Login required and redirect to login page

--- image/views.py
+++ image/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 
 from .models import Image
 
 
-class ImageCreateView(CreateView):
+class ImageCreateView(LoginRequiredMixin, CreateView):
     model = Image
     template_name = 'image/image_create.html'
     fields = ['title', 'image']
