8470c891fe58cf81b7a5b90289dcea0abfe9b187
Weronika <WerRic96@protonmail.com>
2023-10-16 02:31:31 +0200

    Implement basic homepage

--- image_book/urls.py
+++ image_book/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from image.views import ImageListView
+
 urlpatterns = [
+    path('', ImageListView.as_view(), name='image-list'),
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('image/', include('image.urls'))
