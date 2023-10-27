f339b4c8a0dfff04ba2b72d8e3a97c7115ccca86
Weronika <WerRic96@protonmail.com>
2023-10-27 20:13:39 +0200

    Change to staticfiles

--- image_book/settings.py
+++ image_book/settings.py
@@ -138,7 +138,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
