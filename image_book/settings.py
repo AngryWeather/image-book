1dc744844024ef67a13a8b8b734ce371f59262b2
Weronika <WerRic96@protonmail.com>
2023-10-27 20:04:05 +0200

    Set static_root

--- image_book/settings.py
+++ image_book/settings.py
@@ -138,6 +138,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
