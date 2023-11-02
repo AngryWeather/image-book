ae3a74aba6d4a5595a91411a04735365bb8c00cc
Weronika <WerRic96@protonmail.com>
2023-11-02 14:13:33 +0100

    Remove address from allowed hosts

--- image_book/settings.py
+++ image_book/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'imagebook-ho6f.onrender.com']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
